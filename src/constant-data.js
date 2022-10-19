591705954d92072db51d861b19c9ede607949514
Ameybairolu <ameybairolu@gmail.com>
2022-10-19 20:26:30 +0530

    Create a file that stores constant data

---
+++ src/constant-data.js
@@ -0,0 +1,12 @@
+export const navlinkData = [
+    { index: 1, title: "Store" },
+    { index: 2, title: "Mac" },
+    { index: 3, title: "iPad" },
+    { index: 4, title: "iPhone" },
+    { index: 5, title: "Watch" },
+    { index: 6, title: "AirPods" },
+    { index: 7, title: "TV & Home" },
+    { index: 8, title: "Only on Apple" },
+    { index: 9, title: "Accessories" },
+    { index: 10, title: "Support" }
+];
